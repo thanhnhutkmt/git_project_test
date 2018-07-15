a0cad62a56d44d9f9135be233f2a50fba75ce81c
Thunder <seafec2014@gmail.com>
2018-07-15 16:29:22 +0700

    update Realm Db update function

--- app/src/main/java/dagger_project/com/nhut/software/realmdb_myexample1/Dog.java
+++ app/src/main/java/dagger_project/com/nhut/software/realmdb_myexample1/Dog.java
@@ -1,10 +1,38 @@
 package dagger_project.com.nhut.software.realmdb_myexample1;
 
 import io.realm.RealmObject;
+import io.realm.annotations.Index;
+import io.realm.annotations.PrimaryKey;
 
 public class Dog extends RealmObject {
-    public String name;
-    public int age;
+    @PrimaryKey
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     @Override
     public String toString() {
--- app/src/main/java/dagger_project/com/nhut/software/realmdb_myexample1/MainActivity.java
+++ app/src/main/java/dagger_project/com/nhut/software/realmdb_myexample1/MainActivity.java
@@ -1,8 +1,15 @@
 package dagger_project.com.nhut.software.realmdb_myexample1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.realm.RealmResults;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,34 +23,104 @@ import io.realm.RealmResults;
  */
 
 public class MainActivity extends AppCompatActivity {
+    Realm realm;
+    TextView table;
+    EditText nameet, nnameet;
+    Button updateName, updateAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView table = (TextView) findViewById(R.id.table);
+        table = (TextView) findViewById(R.id.table);
+        nameet = (EditText) findViewById(R.id.name);
+        nnameet = (EditText) findViewById(R.id.nname);
+        updateName = (Button) findViewById(R.id.updatename);
+        updateAge = (Button) findViewById(R.id.updateage);
         Dog dog = new Dog();
-        dog.name = "Rex";
-        dog.age = 1;
+        dog.setId(0);
+        dog.setName("Rex");
+        dog.setAge(1);
+
         Dog dog1 = new Dog();
-        dog1.name = "Alaska";
-        dog1.age = 3;
+        dog1.setId(1);
+        dog1.setName("Alaska");
+        dog1.setAge(3);
+
         Dog dog2 = new Dog();
-        dog2.name = "TRex";
-        dog2.age = 4;
+        dog2.setId(2);
+        dog2.setName("TRex");
+        dog2.setAge(4);
+
+        Dog dog5 = new Dog();
+        dog5.setId(3);
+        dog5.setName("TRex1");
+        dog5.setAge(5);
+
+        Dog dog4 = new Dog();
+        dog4.setId(4);
+        dog4.setName("TRex2");
+        dog4.setAge(6);
 
         List<Dog> listDogs = Arrays.asList(dog, dog1, dog2);
 
         Realm.init(this);
-        Realm realm = Realm.getDefaultInstance();
+        realm = Realm.getDefaultInstance();
         realm.beginTransaction();
         realm.deleteAll(); // clean DB
-        realm.copyToRealm(dog2);
-        realm.insert(dog1);
+        realm.copyToRealm(dog4);
+        realm.insert(dog5);
         realm.insert(listDogs);
         realm.commitTransaction();
+        getData();
 
+        updateName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Update name", Toast.LENGTH_SHORT).show();
+                String oldname = nameet.getText().toString();
+                updateDogName(nnameet.getText().toString(), oldname);
+                getData();
+            }
+        });
+        updateAge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Update age", Toast.LENGTH_SHORT).show();
+                String oldname = nameet.getText().toString();
+                updateDogAge((int)(Math.random() * 10), oldname);
+                getData();
+            }
+        });
+    }
+
+    void updateDogName(String newName, String oldName) {
+        realm.beginTransaction();
+        Dog d = getDog(oldName);
+        if (d != null) {
+            d.setName(newName);
+            realm.insertOrUpdate(d);
+        }
+        realm.commitTransaction();
+    }
+
+    void updateDogAge(int age, String oldName) {
+        realm.beginTransaction();
+        Dog d = getDog(oldName);
+        if (d != null) {
+            d.setAge(age);
+            realm.insertOrUpdate(d);
+        }
+        realm.commitTransaction();
+    }
+
+    Dog getDog(String name) {
+        return realm.where(Dog.class).equalTo("name", name).findFirst();
+    }
+
+    void getData() {
         RealmResults<Dog> pups = realm.where(Dog.class)
-            .lessThan("age", 10)
+//            .lessThan("age", 10)
             .findAll();
         table.setText("Result \n");
         for (Dog d : pups.subList(0, pups.size()))
